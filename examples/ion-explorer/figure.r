d6a07478b0709d7c61faf68b088d4311ef2a11b3
Douwe Schulte <d.schulte@uu.nl>
2025-03-21 18:33:59 +0100

    Small figure improvements

--- examples/ion-explorer/figure.r
+++ examples/ion-explorer/figure.r
@@ -1,16 +1,17 @@
 library(ggplot2)
 
-for (file in c("start", "fragment_b", "fragment_y", "fragment_v_0", "fragment_w_0", "fragment_w_1", "fragment_w_2", "fragment_w_3", "fragment_w_4", "fragment_w_5", "fragment_w_6")) {
+for (file in c("start", "fragment_b", "fragment_y", "fragment_v_0", "fragment_w_0", "fragment_w_1", "fragment_w_2", "fragment_w_3", "fragment_w_4", "fragment_w_5", "fragment_w_6", "fragment_precursor")) {
     data = read.csv(paste(file, ".csv", sep=""), header=TRUE);
     # data = data[data$count > 1000,];
 
     plot = ggplot(data, aes(x=mz, y=count, size=avg_intensity, colour=avg_intensity)) +
         geom_point() +
         scale_size_continuous(range = c(0.25, 3)) +
+        xlab("Difference to theoretical ion") +
         theme_bw() 
 
     if (file == "fragment_y") {
-        plot = plot + geom_vline(xintercept=0, linetype="dashed") + geom_vline(xintercept=-18.011+0.984, linetype="dashed") + geom_vline(xintercept=+25.979-18.011, linetype="dashed")
+        plot = plot + geom_vline(xintercept=0, linetype="dashed") + geom_vline(xintercept=+18.011-0.984, linetype="dashed") + geom_vline(xintercept=-25.979+18.011, linetype="dashed")
     }
 
     ggsave(paste(file, ".png", sep=""), plot)
