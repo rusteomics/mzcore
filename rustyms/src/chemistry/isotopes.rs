975b4becf4d8e7f1b8be77255027d621fd8792f6
Douwe Schulte <d.schulte@uu.nl>
2025-08-21 12:09:04 +0200

    Further work on isotope average masses

--- rustyms/src/chemistry/isotopes.rs
+++ rustyms/src/chemistry/isotopes.rs
@@ -114,7 +114,7 @@ impl MolecularFormula {
     /// chosen atoms for lower weighed isotopes into account.
     #[expect(clippy::missing_panics_doc)]
     pub fn isotopic_distribution_with_mass(&self, threshold: f64) -> Array1<(Mass, f64)> {
-        let mut result = arr1(&[(Mass::default(), 1.0_f64)]);
+        let mut full_distribution = arr1(&[(Mass::default(), 1.0_f64)]);
         for (element, isotope, amount) in self.elements() {
             if isotope.is_some() || *amount <= 0 {
                 // TODO: think about negative numbers?
@@ -138,7 +138,7 @@ impl MolecularFormula {
                 .map(|i| (i.0 - base.0, i.1, i.2))
                 .collect_vec();
 
-            let mut first = true; // TODO: initial try to fix the mass output if there are more than 2 isotopes but does not work yet
+            let mut all_isotopes_distribution = arr1(&[(Mass::default(), 0, 1.0_f64)]);
             for (isotope_offset, isotope_mass, isotope_probability) in isotopes {
                 // Generate distribution (take already chosen into account?)
                 let binomial = Binomial::new(amount, isotope_probability);
@@ -162,21 +162,21 @@ impl MolecularFormula {
                     .collect();
 
                 // Make the result fit the data
-                // TODO: think about the need to extend the result length to include the full distribution length
-                // so length (result.len() + ditribution.len() - 1) but keep then filter the output based on the threshold
-                result
+                all_isotopes_distribution
                     .append(
                         Axis(0),
                         Array1::from_elem(
-                            distribution.len().saturating_sub(result.len()),
-                            (Mass::default(), 0.0),
+                            (all_isotopes_distribution.len() + distribution.len() - 1)
+                                .saturating_sub(all_isotopes_distribution.len()),
+                            (Mass::default(), 0, 0.0),
                         )
                         .view(),
                     )
                     .unwrap();
 
                 // Combine distribution with previous distribution
-                let mut new = Array1::from_elem(result.len(), (Mass::default(), 0.0));
+                let mut temporary_stack =
+                    Array1::from_elem(all_isotopes_distribution.len(), (Mass::default(), 0, 0.0));
 
                 dbg!(&distribution);
                 for (shift, shift_probability) in distribution.into_iter().enumerate() {
@@ -184,52 +184,190 @@ impl MolecularFormula {
                     let num = (shift / isotope_offset as usize).min(amount);
 
                     // The mass of this TODO: result in too high masses if multiple isotopes (> 2) exist
-                    let isotope_total_mass = isotope_mass * num as f64
-                        + if first {
-                            base.1 * (amount - num) as f64
-                        } else {
-                            Mass::default()
-                        };
+                    let isotope_total_mass = isotope_mass * num as f64;
                     println!(
                         "E {element} {:.4} i {isotope_offset} p {isotope_probability} s {shift} n {num} p {shift_probability} im {:.4}",
                         isotope_mass.value, isotope_total_mass.value
                     );
-                    new = new
+                    temporary_stack = temporary_stack
                         .into_iter()
                         .zip(
-                            std::iter::repeat_n((Mass::default(), 0.0), shift)
-                                .chain(result.iter().copied()),
+                            std::iter::repeat_n((Mass::default(), 0, 0.0), shift)
+                                .chain(all_isotopes_distribution.iter().copied()),
                         )
-                        .map(|(new, old)| {
+                        .enumerate()
+                        .map(|(index, (new, old))| {
                             (
-                                new.0 + (old.0 + isotope_total_mass) * (old.1 * shift_probability),
-                                old.1.mul_add(shift_probability, new.1),
+                                new.0 + (old.0 + isotope_total_mass) * (old.2 * shift_probability),
+                                new.1 + if index == shift { num } else { 0 },
+                                old.2.mul_add(shift_probability, new.2),
                             )
                         })
                         .collect();
                 }
-
-                result = new
-                    .into_iter()
-                    .map(|(mass, shift_probability)| (mass / shift_probability, shift_probability))
-                    .collect();
-                first = false;
-                dbg!(&result);
+                all_isotopes_distribution = temporary_stack;
             }
+            all_isotopes_distribution = all_isotopes_distribution
+                .into_iter()
+                .filter(|v| v.2 >= threshold)
+                .collect();
+            dbg!(&all_isotopes_distribution);
+            full_distribution
+                .append(
+                    Axis(0),
+                    Array1::from_elem(
+                        all_isotopes_distribution
+                            .len()
+                            .saturating_sub(full_distribution.len()),
+                        (Mass::default(), 0.0),
+                    )
+                    .view(),
+                )
+                .unwrap();
+
+            let mut temporary_stack =
+                Array1::from_elem(full_distribution.len(), (Mass::default(), 0.0));
+            for (shift, (mass, total_isotopes, shift_probability)) in
+                all_isotopes_distribution.into_iter().enumerate()
+            {
+                let mass = mass / shift_probability
+                    + base.1 * amount.saturating_sub(total_isotopes) as f64;
+                temporary_stack = temporary_stack
+                    .into_iter()
+                    .zip(
+                        std::iter::repeat_n((Mass::default(), 0.0), shift)
+                            .chain(full_distribution.iter().copied()),
+                    )
+                    .enumerate()
+                    .map(|(index, (new, old))| {
+                        (
+                            new.0
+                                + if index == shift {
+                                    mass + old.0
+                                } else {
+                                    Mass::default()
+                                },
+                            old.1.mul_add(shift_probability, new.1),
+                        )
+                    })
+                    .collect();
+            }
+            full_distribution = temporary_stack;
+            full_distribution = full_distribution
+                .into_iter()
+                .filter(|v| v.1 >= threshold)
+                .collect();
+            dbg!(&full_distribution);
         }
-        result
+        full_distribution
     }
 }
 
 #[cfg(test)]
+#[allow(clippy::missing_panics_doc)]
 mod test {
     #[test]
     fn with_mass() {
-        let formula = molecular_formula!(C 18 H 33 S 2 O 6 N 8);
+        // let formula = molecular_formula!(C 18 H 33 S 2 O 6 N 8);
+        // let formula = molecular_formula!(O 100);
+        // let formula = molecular_formula!(Se 10);
+        let formula = molecular_formula!(H 202 O 10);
 
         let distribution = formula.isotopic_distribution_with_mass(0.001);
         dbg!(distribution);
 
         todo!();
+    }
+
+    #[test]
+    fn h_mass_and_probability() {
+        let formula = molecular_formula!(H 100);
+        let expected = vec![
+            (molecular_formula!([1 H 100]).monoisotopic_mass(), 0.985_604),
+            (
+                molecular_formula!([1 H 99] [2 H 1]).monoisotopic_mass(),
+                0.014_293,
+            ),
+        ];
+        let distribution = formula.isotopic_distribution_with_mass(0.001);
+
+        assert_eq!(distribution.len(), expected.len());
+        for (gotten, expected) in distribution.iter().zip(expected) {
+            assert!((gotten.0.value - expected.0.value).abs() < 1E-4);
+            assert!((gotten.1 - expected.1).abs() < 1E-4);
+        }
+    }
+
+    #[test]
+    fn o_mass_and_probability() {
+        let formula = molecular_formula!(O 25);
+        let expected = vec![
+            (molecular_formula!([16 O 25]).monoisotopic_mass(), 0.941_043),
+            (
+                molecular_formula!([16 O 24] [17 O 1]).monoisotopic_mass(),
+                0.009_026,
+            ),
+            (
+                molecular_formula!([16 O 24] [18 O 1]).monoisotopic_mass(),
+                0.048_209,
+            ),
+            (
+                molecular_formula!([16 O 23] [17 O 1] [18 O 1]).monoisotopic_mass(),
+                0.001_185,
+            ),
+        ];
+        let distribution = formula.isotopic_distribution_with_mass(0.001);
+        dbg!(&distribution);
+        assert_eq!(distribution.len(), expected.len());
+        for (offset, (gotten, expected)) in distribution.iter().zip(expected).enumerate() {
+            assert!(
+                (gotten.0.value - expected.0.value).abs() < 1E-4,
+                "Wrong mass at offset: {offset} Expected: {:.6} Gotten: {:.6}",
+                expected.0.value,
+                gotten.0.value,
+            );
+            assert!(
+                (gotten.1 - expected.1).abs() < 1E-4,
+                "Wrong probability at offset: {offset}"
+            );
+        }
+    }
+
+    #[test]
+    fn ho_mass_and_probability() {
+        let formula = molecular_formula!(H 100 O 25);
+        let expected = vec![
+            (
+                molecular_formula!([1 H 100] [16 O 25]).monoisotopic_mass(),
+                0.927_495,
+            ),
+            (
+                molecular_formula!([1 H 100] [16 O 24] [17 O 1]).monoisotopic_mass(), // TODO: is mixture
+                0.022_346,
+            ),
+            (
+                molecular_formula!([1 H 100] [16 O 24] [18 O 1]).monoisotopic_mass(), // TODO: is mixture
+                0.047_515,
+            ),
+            (
+                molecular_formula!([1 H 100] [16 O 23] [17 O 1] [18 O 1]).monoisotopic_mass(), // TODO: is mixture
+                0.001_145,
+            ),
+        ];
+        let distribution = formula.isotopic_distribution_with_mass(0.001);
+        dbg!(&distribution);
+        assert_eq!(distribution.len(), expected.len());
+        for (offset, (gotten, expected)) in distribution.iter().zip(expected).enumerate() {
+            assert!(
+                (gotten.0.value - expected.0.value).abs() < 1E-4,
+                "Wrong mass at offset: {offset} Expected: {:.6} Gotten: {:.6}",
+                expected.0.value,
+                gotten.0.value,
+            );
+            assert!(
+                (gotten.1 - expected.1).abs() < 1E-4,
+                "Wrong probability at offset: {offset}"
+            );
+        }
     }
 }
