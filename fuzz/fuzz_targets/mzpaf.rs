612926ea4e90fc31c179c87efddde3ada6c443f9
Douwe Schulte <d.schulte@uu.nl>
2025-10-23 23:22:53 +0200

    Added more mzPAF crashes

--- fuzz/fuzz_targets/mzpaf.rs
+++ fuzz/fuzz_targets/mzpaf.rs
@@ -5,8 +5,7 @@ use mzcore::prelude::CompoundPeptidoformIon;
 fn main() {
     fuzz!(|data: &[u8]| {
         if let Ok(s) = std::str::from_utf8(data) {
-            let _unused =
-                mzannotate::fragment::parse_mz_paf(s, None, &CompoundPeptidoformIon::default());
+            let _unused = mzannotate::fragment::Fragment::mz_paf(s, None, &[]);
         }
     });
 }
