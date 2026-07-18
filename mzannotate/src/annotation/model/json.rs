80d6a2f77f2b869f7094410552c6eefd384fe527
Douwe Schulte <d.schulte@uu.nl>
2026-07-18 21:52:34 +0200

    Quick fix

--- mzannotate/src/annotation/model/json.rs
+++ mzannotate/src/annotation/model/json.rs
@@ -113,7 +113,7 @@ impl ParseJson for FragmentationModel {
                         context(&map),
                     )
                 })?)?,
-                internal: map.remove("immonium").map(|v| Option::from_json_value(v)).transpose()?.flatten(),
+                internal: map.remove("internal").map(|v| Option::from_json_value(v)).transpose()?.flatten(),
                 immonium: Option::from_json_value(map.remove("immonium").ok_or_else(|| {
                     BoxedError::new(
                         BasicKind::Error,
